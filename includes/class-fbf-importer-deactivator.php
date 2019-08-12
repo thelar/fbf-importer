e442dfa73ca3261b06f102c7ba3b9cf6f97960c2
Kevin Price-Ward <kevin.price-ward@chapteragency.co.uk>
2019-08-12 12:16:34 +0100

    first commit

---
+++ includes/class-fbf-importer-deactivator.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Fired during plugin deactivation
+ *
+ * @link       https://www.chapteragency.com
+ * @since      1.0.0
+ *
+ * @package    Fbf_Importer
+ * @subpackage Fbf_Importer/includes
+ */
+
+/**
+ * Fired during plugin deactivation.
+ *
+ * This class defines all code necessary to run during the plugin's deactivation.
+ *
+ * @since      1.0.0
+ * @package    Fbf_Importer
+ * @subpackage Fbf_Importer/includes
+ * @author     Kevin Price-Ward <kevin.price-ward@chapteragency.com>
+ */
+class Fbf_Importer_Deactivator {
+
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    1.0.0
+	 */
+	public static function deactivate() {
+
+	}
+
+}
