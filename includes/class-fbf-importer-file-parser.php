db7b9adef168e60d22c94c7b9664c76cb20eb3e0
Kevin Price-Ward <kevin.price-ward@4x4tyres.co.uk>
2021-01-05 15:00:13 +0000

    filter out lowest supplier cost of zero

--- includes/class-fbf-importer-file-parser.php
+++ includes/class-fbf-importer-file-parser.php
@@ -781,11 +781,15 @@ class Fbf_Importer_File_Parser {
                 //If we get here and $cost is still null look for cheapest supplier but WITHOUT stock
                 if($cost===null){
                     foreach($item['Suppliers'] as $supplier){
-                        if($cost===null){
-                            $cost = (float) $supplier['Supplier Cost Price'];
+                        if($cost===null){ // First time
+                            if((float) $supplier['Supplier Cost Price'] > 0){
+                                $cost = (float) $supplier['Supplier Cost Price'];
+                            }
                         }else{
                             if((float) $supplier['Supplier Cost Price'] < $cost){
-                                $cost = (float) $supplier['Supplier Cost Price'];
+                                if((float) $supplier['Supplier Cost Price'] > 0) {
+                                    $cost = (float)$supplier['Supplier Cost Price'];
+                                }
                             }
                         }
                     }
