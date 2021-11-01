a10eda91690603ba3764bc254503d4c8250376e9
Kevin Price-Ward <kevin.price-ward@4x4tyres.co.uk>
2021-11-01 17:17:26 +0000

    modify stock for eden

--- includes/class-fbf-importer-process-stock.php
+++ includes/class-fbf-importer-process-stock.php
@@ -104,7 +104,7 @@ class Fbf_Importer_Stock_Processor
         $supplier_array[7]['read_filename'] = "edentyresales.csv";
         $supplier_array[7]['write_filename'] = "edentyresales";
         $supplier_array[7]['cell_1a'] = "EDENTYRE";
-        $supplier_array[7]['data_start_row'] = "2";
+        $supplier_array[7]['data_start_row'] = "1";
         $supplier_array[7]['mapping_array'] = array('2', '0', '4', '17');
         $supplier_array[7]['delimiter'] = ",";
 
