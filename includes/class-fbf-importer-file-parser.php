25a03adedf84fdcfd2b7faec09868abb183b32ce
Kevin Price-Ward <kevin.price-ward@4x4tyres.co.uk>
2022-01-20 14:03:15 +0000

    Add white lettering back in to stages

--- includes/class-fbf-importer-file-parser.php
+++ includes/class-fbf-importer-file-parser.php
@@ -23,7 +23,7 @@ class Fbf_Importer_File_Parser {
         'build_stock_array',
         'get_rsp_rules',
         'duplicate_white_lettering_items',
-        //'import_stock_white',
+        'import_stock_white',
         'import_stock',
         //'update_ebay_packages',
         'rotate_stock_files',
