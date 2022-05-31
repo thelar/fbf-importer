efc25dbed893bf1791468b1cb2a607e70ae71814
Kevin Price-Ward <kevin.price-ward@4x4tyres.co.uk>
2022-05-31 08:48:20 +0100

    chore: remove bond from supplier processing

--- includes/class-fbf-importer-process-stock.php
+++ includes/class-fbf-importer-process-stock.php
@@ -71,13 +71,13 @@ class Fbf_Importer_Stock_Processor
         $supplier_array[2]['mapping_array'] = array('0', '16', '13', '15');
         $supplier_array[2]['delimiter'] = ",";
 
-        /*$supplier_array[3]['name'] = "MALVERN";
+        $supplier_array[3]['name'] = "MALVERN";
         $supplier_array[3]['read_filename'] = "cooper.xls";
         $supplier_array[3]['write_filename'] = "malvern";
         $supplier_array[3]['cell_1a'] = "MALVERN";
         $supplier_array[3]['data_start_row'] = "3";
         $supplier_array[3]['mapping_array'] = array('0', '1', '3', '6');
-        $supplier_array[3]['delimiter'] = ",";*/
+        $supplier_array[3]['delimiter'] = ",";
 
         /*$supplier_array[4]['name'] = "DELDO";
         $supplier_array[4]['read_filename'] = "deldo47545.csv"; //"TOPGEAR.csv";
