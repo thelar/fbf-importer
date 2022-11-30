5dcb27deb0d5dbe08c1abb9b406a45373a377ea2
Kevin Price-Ward <kevin.price-ward@4x4tyres.co.uk>
2022-11-30 21:17:59 +0000

    bail after 2

--- includes/class-fbf-importer-file-parser.php
+++ includes/class-fbf-importer-file-parser.php
@@ -27,10 +27,10 @@ class Fbf_Importer_File_Parser {
         'build_price_match_data',
         'duplicate_white_lettering_items',
         'setup_products_to_hide',
-        'import_stock_white',
+        //'import_stock_white',
         'import_stock',
-        'hide_products',
-        'hide_products_without_images',
+        //'hide_products',
+        //'hide_products_without_images',
         'update_ebay_packages',
         'rotate_stock_files',
         'write_rsp_xml',
@@ -231,13 +231,13 @@ class Fbf_Importer_File_Parser {
 
         $counter = 0;
 
-        //$max = 2; // -- uncomment to only import $max products for testing purposes
+        $max = 2; // -- uncomment to only import $max products for testing purposes
 
 
         foreach ($list as $item) {
-            /*if($counter >= $max){ // -- uncomment to only import $max products for testing purposes
+            if($counter >= $max){ // -- uncomment to only import $max products for testing purposes
                 break;
-            }*/
+            }
             $sku = (string)$item['Product Code'];
             $name_gpf = null; // need to set to null for looping
 
