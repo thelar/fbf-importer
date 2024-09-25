867d265d08ae68dc40b6c9f3a21ab065af2ed197
Kevin Price-Ward <kevin.price-ward@4x4tyres.co.uk>
2024-09-25 10:23:57 +0100

    TYR-398 #comment Add check to see if other import is running before allowing API to start import

--- includes/class-fbf-importer-api.php
+++ includes/class-fbf-importer-api.php
@@ -69,14 +69,16 @@ class Fbf_Importer_Api extends Fbf_Importer_Admin
             exit;
         }else if($wp->request == 'api/v2/import_pimberly'){
             $options = get_option($this->plugin_name . '-mts-ow', ['status' => 'STOPPED']);
-            if($options['status']==='STOPPED'){
+            $boughto_options = get_option($this->plugin_name . 'boughto-ow', ['status' => 'STOPPED']); // Need to check if Boughto import is stopped as well because OW will error if both run at the same time
+            if($options['status']==='STOPPED' && $boughto_options['status']==='STOPPED'){
                 $old_options = get_option($this->plugin_name . '-mts-ow', ['status' => 'STOPPED']);
                 update_option($this->plugin_name . '-mts-ow', ['status' => 'READY', 'log_id' => $old_options['log_id']]);
             }
             exit;
         }else if($wp->request == 'api/v2/import_boughto'){
             $options = get_option($this->plugin_name . 'boughto-ow', ['status' => 'STOPPED']);
-            if($options['status']==='STOPPED'){
+            $pimberly_options = get_option($this->plugin_name . '-mts-ow', ['status' => 'STOPPED']); // Need to check if Pimbery import is stopped as well because OW will error if both run at the same time
+            if($options['status']==='STOPPED' && $pimberly_options['status']==='STOPPED'){
                 $old_options = get_option($this->plugin_name . 'boughto-ow', ['status' => 'STOPPED']);
                 update_option($this->plugin_name . 'boughto-ow', ['status' => 'READY', 'log_id' => $old_options['log_id']]);
             }
