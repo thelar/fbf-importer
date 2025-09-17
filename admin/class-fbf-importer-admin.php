7636061aceb72e1e0670ec18dba3f43445ad69ef
Kevin Price-Ward <kevin.price-ward@4x4tyres.co.uk>
2025-09-17 11:06:45 +0100

    TYR-553 #comment importer tidy up

--- admin/class-fbf-importer-admin.php
+++ admin/class-fbf-importer-admin.php
@@ -1026,11 +1026,12 @@ class Fbf_Importer_Admin {
 						unset($option['item_start_time']);
 					}
 					update_option($this->plugin_name, $option);*/
-					$output = shell_exec( sprintf( 'kill -9 %s', $pid ) );
+					$pid = 1852797;
+					$output = exec( sprintf( 'kill -9 %s', $pid ), $e_output, $result_code );
 					echo '<pre>';
 					print_r($output);
-					/*print_r($e_output);
-					print_r($result_code);*/
+					print_r($e_output);
+					print_r($result_code);
 					echo '<pre>';
 				}
 			}
